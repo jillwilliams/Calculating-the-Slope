a462b8e53b9ae6d789c6a0d74e0202f3a0d7cd3d
jillwilliams <codeGeek.03.14@gmail.com>
2019-11-15 10:59:30 -0800

    draft

---
+++ script.js
@@ -0,0 +1,20 @@
+const myProblem = () => {
+    return "-1";
+};
+const myCalc = () => {
+    document.getElementById("demo").innerHTML = myProblem();
+};
+
+const myProblem2 = () => {
+    return "-7/10";
+};
+const myCalc2 = () => {
+    document.getElementById("demo2").innerHTML = myProblem2();
+};
+
+const myProblem3 = () => {
+    return "12/10";
+};
+const myCalc3 = () => {
+    document.getElementById("demo3").innerHTML = myProblem3();
+};
